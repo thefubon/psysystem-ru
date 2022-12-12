c0e24b78e754a483e847bd9fe533e39e68b7994d
Fubon - Website Design & Frontend Development <43171309+thefubon@users.noreply.github.com>
2022-12-12 15:41:24 +0300

    upd

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,7 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+const colors = require('tailwindcss/colors')
 module.exports = {
   content: [
     "./components/**/*.{js,vue,ts}",
@@ -9,7 +12,36 @@ module.exports = {
     "./app.vue",
   ],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1rem',
+        xs: '1rem',
+        sm: '1rem',
+        lg: '1rem',
+        xl: '1rem',
+      },
+    },
+    screens: {
+      xs: '480px',
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1536px',
+    },
+    extend: {
+      colors: {
+        primary: colors.yellow,
+        secondary: colors.slate,
+      },
+      spacing: {
+        13: '3.25rem',
+        15: '3.75rem',
+        128: '32rem',
+        144: '36rem',
+      },
+    },
   },
   plugins: [],
 }
