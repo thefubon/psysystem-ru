2a8fdfb96f501fd25938a2f50793cb172b558f96
Fubon - Website Design & Frontend Development <43171309+thefubon@users.noreply.github.com>
2022-12-20 00:32:43 +0300

    upd

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -2,7 +2,7 @@
   <header class="sticky top-0 z-10 border-b bg-white">
     <div class="container flex h-16 md:h-20 items-center justify-between font-medium">
       <div>
-        <a href="https://fubon.ru">
+        <a class="inline-block" href="https://fubon.ru">
           <h1 class="sr-only">Fubon</h1>
           <img class="h-[28px] w-[113px] min-w-fit" src="/img/logo.svg" alt="Fubon" width="113" height="28" aria-hidden="true" />
         </a>
