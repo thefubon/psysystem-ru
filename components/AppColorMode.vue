f587d37e92852af0f63c1a684aa217e1299dba02
Fubon - Website Design & Frontend Development <43171309+thefubon@users.noreply.github.com>
2022-12-22 19:44:47 +0300

    upd

--- components/AppColorMode.vue
+++ components/AppColorMode.vue
@@ -2,15 +2,15 @@
   <div class="hs-dropdown relative inline-flex [--placement:top-right]">
     <button id="hs-dropup" type="button" class="hs-dropdown-toggle inline-flex gap-x-2">
       <span v-if="$colorMode.preference === 'system'">
-        <Icon class="text-slate-300" name="uil:desktop" size="24" />
+        <Icon class="text-slate-300" name="uil:desktop" size="26" />
       </span>
 
       <span v-else-if="$colorMode.preference === 'light'">
-        <Icon class="text-slate-300" name="uil:sun" size="24" />
+        <Icon class="text-slate-300" name="uil:sun" size="26" />
       </span>
 
       <span v-else-if="$colorMode.preference === 'dark'">
-        <Icon class="text-slate-300" name="uil:moon" size="24" />
+        <Icon class="text-slate-300" name="uil:moon" size="26" />
       </span>
     </button>
 
