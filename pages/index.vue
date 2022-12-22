69981c93c07843193496d237642e0280861eb0e3
Fubon - Website Design & Frontend Development <43171309+thefubon@users.noreply.github.com>
2022-12-22 14:34:57 +0300

    upd

--- pages/index.vue
+++ pages/index.vue
@@ -20,7 +20,7 @@
             <div class="hover:cursor-pointer relative group overflow-hidden rounded-lg" @click="selectSound(indexo)">
               <span class="opacity-0 group-hover:opacity-100 duration-150 absolute inset-0 bg-black/50"></span>
 
-              <NuxtImg provider="cloudinary" format="webp" sizes="sm:760px" quality="100" :src="audio.cover" :alt="audio.name" width="480" height="480" />
+              <NuxtImg provider="cloudinary" format="webp" sizes="sm:250px" quality="80" :src="audio.cover" :alt="audio.name" width="250" height="250" />
 
               <!-- NEW STATUS -->
               <span class="absolute top-3 right-3 bg-yellow-400 px-1.5 text-black rounded-full text-xs" v-if="audio.new">New</span>
@@ -106,7 +106,7 @@
         <div class="flex w-full md:w-auto items-center gap-4">
           <!-- COVER -->
           <div class="flex-1 flex items-center gap-2 md:gap-4" v-for="(audio,indexo) in audios.slice(index, index + 1)" :key="indexo">
-            <NuxtImg class="rounded-md" provider="cloudinary" format="webp" sizes="sm:80px" quality="80" :src="audio.cover" :alt="audio.name" width="40" height="40" />
+            <NuxtImg class="rounded-md" provider="cloudinary" format="webp" sizes="sm:48px" quality="80" :src="audio.cover" :alt="audio.name" width="40" height="40" />
 
             <!-- ABOUT -->
             <div class="flex flex-col font-semibold">
