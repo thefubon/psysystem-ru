c6a5f2a7d645e9b2e5d803c695da8513a151aaac
Fubon - Website Design & Frontend Development <43171309+thefubon@users.noreply.github.com>
2022-12-25 18:42:31 +0300

    upd

--- pages/index.vue
+++ pages/index.vue
@@ -40,7 +40,7 @@
               <p class="text-xs text-gray-500 dark:text-gray-300/75">Выпущено: {{audio.date}}</p>
             </div>
 
-            <div>
+            <!-- <div>
               <button type="button" class="py-3 px-4 inline-flex justify-center items-center gap-2 rounded-md border border-transparent font-semibold bg-blue-500 text-white hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-all text-sm dark:focus:ring-offset-gray-800" :data-hs-overlay="'#id-' + indexo + 1">
                 Vertically centered modal
               </button>
@@ -75,7 +75,7 @@
                   </div>
                 </div>
               </div>
-            </div>
+            </div> -->
 
           </div>
         </div>
